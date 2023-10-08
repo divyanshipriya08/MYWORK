1d452bc7a3c38c583b4a0c9a1e3b950240a57bf6
divyanshipriya08 <137182937+divyanshipriya08@users.noreply.github.com>
2023-10-08 19:59:39 +0530

    Add files via upload

---
+++ starpattern.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,row;
+
+    printf(" enter the number of the row:");
+    scanf(" %d",&row ,i++);
+
+        for(i=1;i<=9;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+
+
+    }
+    return 0;
+}
