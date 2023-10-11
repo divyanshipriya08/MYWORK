7b61350faf9ca2cdf8ec0a683b2cc072881e4102
divyanshipriya08 <137182937+divyanshipriya08@users.noreply.github.com>
2023-10-12 00:36:14 +0530

    Add files via upload

---
+++ reverse number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  int n, reversed = 0, remainder, original;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    original = n;
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+printf("reverse of the given number is:%d\n",reversed);
+
+
+    return 0;
+}
